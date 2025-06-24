c844211a1c787ed07e93056a36f2b02464cb9eed
Jana Mitrovic <mitrovicjanakv@gmail.com>
2025-06-25 01:12:47 +0200

    testiranje pre i post process funkcija

--- BaseColor.py
+++ BaseColor.py
@@ -3,6 +3,7 @@ from torch import nn
 import numpy as np
 from PIL import Image
 from skimage import color
+import torch.nn.functional as F
 
 class BaseColor(nn.Module):
 	def __init__(self):
@@ -30,10 +31,10 @@ def load_img(img_path):
 		out_np = np.tile(out_np[:,:,None],3)
 	return out_np
 
-def resize_img(img, HW=(256,256), resample=3):
+def resize_img(img, HW=(256,256), resample=4):
 	return np.asarray(Image.fromarray(img).resize((HW[1],HW[0]), resample=resample))
 
-def preprocess_img(img_rgb_orig, HW=(256,256), resample=3):
+def preprocess_img(img_rgb_orig, HW=(256,256), resample=4):
 	img_rgb_rs = resize_img(img_rgb_orig, HW=HW, resample=resample)
 	
 	img_lab_orig = color.rgb2lab(img_rgb_orig)
@@ -46,3 +47,19 @@ def preprocess_img(img_rgb_orig, HW=(256,256), resample=3):
 	tens_rs_l = torch.Tensor(img_l_rs)[None,None,:,:]
 
 	return (tens_orig_l, tens_rs_l)
+
+def postprocess_tens(tens_orig_l, out_ab, mode='bilinear'):
+	# tens_orig_l 	1 x 1 x H_orig x W_orig
+	# out_ab 		1 x 2 x H x W
+
+	HW_orig = tens_orig_l.shape[2:]
+	HW = out_ab.shape[2:]
+
+	# call resize function if needed
+	if(HW_orig[0]!=HW[0] or HW_orig[1]!=HW[1]):
+		out_ab_orig = F.interpolate(out_ab, size=HW_orig, mode='bilinear')
+	else:
+		out_ab_orig = out_ab
+
+	out_lab_orig = torch.cat((tens_orig_l, out_ab_orig), dim=1)
+	return color.lab2rgb(out_lab_orig.data.cpu().numpy()[0,...].transpose((1,2,0)))
