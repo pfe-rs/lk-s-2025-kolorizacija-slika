a916370e2734fa1e83241db64cd510d877092d9f
Jana Mitrovic <mitrovicjanakv@gmail.com>
2025-06-25 01:52:34 +0200

    uklonjeni slojevi 6 i 7 u generatoru

--- generator.py
+++ generator.py
@@ -55,25 +55,25 @@ class Generator(BaseColor):
             norm_layer(512)
         ]
 
-        model6 = [
-            nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),
-            nn.LeakyReLU(True),
-            nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),
-            nn.LeakyReLU(True),
-            nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),
-            nn.LeakyReLU(True),
-            norm_layer(512)
-        ]
+        # model6 = [
+        #     nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),
+        #     nn.LeakyReLU(True),
+        #     nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),
+        #     nn.LeakyReLU(True),
+        #     nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),
+        #     nn.LeakyReLU(True),
+        #     norm_layer(512)
+        # ]
 
-        model7 = [
-            nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),
-            nn.LeakyReLU(True),
-            nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),
-            nn.LeakyReLU(True),
-            nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),
-            nn.LeakyReLU(True),
-            norm_layer(512)
-        ]
+        # model7 = [
+        #     nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),
+        #     nn.LeakyReLU(True),
+        #     nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),
+        #     nn.LeakyReLU(True),
+        #     nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),
+        #     nn.LeakyReLU(True),
+        #     norm_layer(512)
+        # ]
 
         model8 = [
             nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=True),
@@ -100,8 +100,8 @@ class Generator(BaseColor):
         self.model3 = nn.Sequential(*model3)
         self.model4 = nn.Sequential(*model4)
         self.model5 = nn.Sequential(*model5)
-        self.model6 = nn.Sequential(*model6)
-        self.model7 = nn.Sequential(*model7)
+        # self.model6 = nn.Sequential(*model6)
+        # self.model7 = nn.Sequential(*model7)
         self.model8 = nn.Sequential(*model8)
         self.model9 = nn.Sequential(*model9)
 
@@ -115,10 +115,10 @@ class Generator(BaseColor):
         conv3_3 = self.model3(conv2_2)
         conv4_3 = self.model4(conv3_3)
         conv5_3 = self.model5(conv4_3)
-        conv6_3 = self.model6(conv5_3)
-        conv7_3 = self.model7(conv6_3)
-        conv8_3 = self.model8(conv7_3)
-        conv9_3 = self.model9(conv8_3)
+        # conv6_3 = self.model6(conv5_3)
+        # conv7_3 = self.model7(conv6_3)
+        conv8_3 = self.model8(conv5_3)
+        conv9_3 = self.model9(conv5_3)
         out_reg = self.model_out(self.softmax(conv9_3))
         return self.unnormalize_ab(self.upsample4(out_reg))
     
