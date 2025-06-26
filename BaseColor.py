0ac589ceec2272f2a08e9f14361778ef87146649
Jana Mitrovic <mitrovicjanakv@gmail.com>
2025-06-26 16:07:08 +0200

    demo trening petlje i dodata normalizacija u pre/postprocessing

--- BaseColor.py
+++ BaseColor.py
@@ -34,28 +34,32 @@ def load_img(img_path):
 def resize_img(img, HW=(256,256), resample=4):
 	return np.asarray(Image.fromarray(img).resize((HW[1],HW[0]), resample=resample))
 
-def preprocess_img(img_rgb_orig, HW=(256,256), resample=4):   # ZA UNOS U GENERATOR - RGB SLIKA U TENSORE NORMALIZOVANA L I AB KANALA
-	img_rgb = resize_img(img_rgb_orig, HW=HW, resample=resample)
-	
-	img_lab = color.rgb2lab(img_rgb).astype(np.float32)
+def preprocess_img(img_rgb_orig, HW=(256,256), resample=4):
+    basecolor = BaseColor()
 
-	img_l = img_lab[:,:,0]
-	img_ab = img_lab[:,:,1:3]
+    img_rgb = resize_img(img_rgb_orig, HW=HW, resample=resample)
+    img_lab = color.rgb2lab(img_rgb).astype(np.float32)
 
-	tens_l = torch.Tensor(img_l)[None,None,:,:]
-	tens_l = BaseColor.normalize_l(tens_l)  # 1 x 1 x H x W
+    img_l = img_lab[:,:,0]
+    img_ab = img_lab[:,:,1:3]
 
-	tens_ab = torch.tensor(img_ab).permute(2,0,1).float()  # 1 x 2 x H x W
-	tens_ab = BaseColor.normalize_ab(tens_ab)
+    tens_l = torch.tensor(img_l)[None, None, :, :]
+    tens_l = basecolor.normalize_l(tens_l)
 
-	return (tens_l, tens_ab,img_lab)
+    tens_ab = torch.tensor(img_ab).permute(2, 0, 1).unsqueeze(0)  # (1, 2, H, W)
+    tens_ab = basecolor.normalize_ab(tens_ab)
+
+    return tens_l, tens_ab, img_lab
+
 
 def postprocess_img(tens_l, tens_gen_ab, mode='bilinear'):
 
     #tens l: tensor oblika (1, 1, H_orig, W_orig)
    # out_ab: tensor oblika (1, 2, H, W)
    # Vraca: tensor oblika (1, 3, H_orig, W_orig) -> L + ab (resized)
-
+    basecolor = BaseColor()
+    tens_l = basecolor.unnormalize_l(tens_l)  # L kanal
+    tens_gen_ab = basecolor.unnormalize_ab(tens_gen_ab)
     HW_orig = tens_l.shape[2:]
     HW_ab = tens_gen_ab.shape[2:]
 
